a30445834979fcdbcd873cfb9ad5b976e11aea3e
Ani <Ani@DESKTOP-JTOGLPH>
2020-09-09 16:36:22 +0800

    Updating project

---
+++ NeuralMassModel/neural_population_level/population_network_param.m
@@ -0,0 +1,51 @@
+function [param] = population_network_param(param)
+%% Basic properties for the connectivity and delays of a neural mass model population 
+%% Inputs
+% param - parameters from "unit_response_param"
+%%
+%% Outputs
+% H_excitatory - excitatory layer intrinsic amplitude parameter    
+
+%%
+%% Loading unit response parameters 
+sigma_dwSte4        = param.spectral_response.stochastic_inputs.sigma_dwSte4;
+tau_unit            = param.spectral_response.tau_unit;
+%% Defining population parameters
+Nunit               = 10000;
+Ipop                = eye(Nunit);
+nu_pop              = 10; % population frequency (Hz) 
+taumin              = 3*max(tau_unit);
+connections         = [1 2; 2 1];
+extensions          = [ceil(Nunit/3); ceil(Nunit/3); Nunit - 2*ceil(Nunit/3)];
+options.connections = connections;
+options.extensions  = extensions;
+%% Creating syntetic directed connectivity and delays  
+% Theta = (I-K)'*diag(1/sigma_dwpop)*(I-K)
+[~,Theta_pop]       = hggm(Nunit,options);
+% Theta_pop = U*D*U'
+% Theta_pop = (U*sqrt(D))*(U*sqrt(D))'
+[U,D]               = svd(Theta_pop);
+Ddiag               = diag(D);
+Ddiag_sqrt          = sqrt(Ddiag);
+U                   = U*diag(Ddiag_sqrt);
+% Theta_pop = (U*sqrt(D)*sqrt(Sigma_pop))*(1/Sigma_pop)*(U*sqrt(D)*sqrt(Sigma_pop))'
+sigma_pop_sqrt      = sqrt(sigma_dwSte4)*ones(Nunit,1);
+Sigma_pop_sqrt      = diag(sigma_pop_sqrt);
+U                   = U*Sigma_pop_sqrt;
+% K = I - (U*sqrt(D)*sqrt(Sigma_pop))'
+K0                  = Ipop - U';
+% K(nu_pop) = FT[abs(K).*delta(t-delays_pop)]
+tau0                = (-angle(K0) + pi)/(2*pi*nu_pop) + taumin;
+tau0                = tau0 - diag(diag(tau0));
+sigma0              = tau0/3 + eye(length(tau0));
+taumax              = 1.5*max(tau0(:));
+K0                  = real(K0) - diag(diag(real(K0)));
+%% Saving parameters
+param.population_network.Nunit  = Nunit;
+param.population_network.Ipop   = Ipop;
+param.population_network.nu_pop = nu_pop;
+param.population_network.K0     = K0;
+param.population_network.tau0   = tau0;
+param.population_network.taumax = taumax;
+param.population_network.sigma0 = sigma0;
+end
---
+++ NeuralMassModel/neural_unit_level/unit_jansen_and_rit_param.m
@@ -0,0 +1,54 @@
+function param = unit_jansen_and_rit_param(param)
+%% Basic properties of the Jansen and Rit model
+%% Inputs
+% param - empty structure
+%%
+%% Outputs
+% Nlayers - number of neural layers 
+% Pyr23 - Pyramidal layer 2 and 3 
+% Inh23 - Inhibitory interneurons 2 and 3
+% Ste4 - Spiny Stellate cells 
+% e0 - Maximum of the sigmoid function
+% v0 - Position of the threshold of the sigmoid function
+% r  - Steepness of the sigmoid function
+% C - interlayer coupling strength 
+% Cmask - mask of coupling strength 
+%% Defining Layers
+Nlayers                              = 3;
+Pyr23                                = 1;
+Inh23                                = 2; 
+Ste4                                 = 3; 
+param.jansen_and_rit.layers.Nlayers  = Nlayers;
+param.jansen_and_rit.layers.Pyr23    = Pyr23;  
+param.jansen_and_rit.layers.Inh23    = Inh23;  
+param.jansen_and_rit.layers.Ste4     = Ste4; 
+%% Defining Sigmoid  
+e0                                   = 2.5;
+v0                                   = 6;
+r                                    = 0.56;
+param.jansen_and_rit.sigmoid.e0      = e0; 
+param.jansen_and_rit.sigmoid.v0      = v0; 
+param.jansen_and_rit.sigmoid.r       = r;
+%% Defining interlayer coupling strength
+C                                    = zeros(Nlayers, Nlayers);
+%                                      |0  -c4 c2|
+% C                                  = |c3  0  0 |
+%                                      |c1  0  0 |
+% Reference values for coupling strength
+c                                    = 135; 
+c1                                   = 1*c; 
+c2                                   = 0.8*c; 
+c3                                   = 0.25*c; 
+c4                                   = 0.25*c;
+% Filling coupling matrix C 
+C(Ste4, Pyr23)                       = c1; 
+C(Inh23, Pyr23)                      = c3;  
+C(Pyr23, Ste4)                       = c2;
+C(Pyr23, Inh23)                      = -c4;
+% Filling coupling matrix Cmask
+Cmask                                = zeros(Nlayers,Nlayers);
+Cmask(C~=0)                          = 1;
+%% Saving parameters
+param.jansen_and_rit.coupling.C      = C;
+param.jansen_and_rit.coupling.Cmask  = Cmask;
+end
